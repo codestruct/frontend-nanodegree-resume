f3636c70b8069b92468d449255e2fc839b7bba68
codestrunct <hello@helenshiu.com>
2015-10-22 13:53:14 -0400

    Update completion of map and all section encapsulation
    All sections in resume are encapsulated in function().
    Google map is showing.
    Info windows for all the pins on there.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//Create bio object
+//bio object -----------------------------------------------------//
 var bio = {
 	"name":"Helen Shiu",
 	"role":"Designer & Developer",
@@ -10,7 +10,8 @@ var bio = {
 		"github" : "https://github.com/quilt9",
 		"twitter" : "@quilt9design",
 		"linkedin" : "https://www.linkedin.com/in/helenshiu",
-		"location" : "Toronto, Canada"
+		"city" : "Toronto, Canada",
+		"location" : "1048 Broadview ave., Toronto, Canada"
 		},
 	"welcome" : "Hello, my name is Helen. I am an UI Designer and Developer who is all about creating good online user experience.",
 	"skills" : ["Visual Design", "HTML", "CSS", "Responsive Design", "BootStrap", "Wireframe"],
@@ -22,8 +23,7 @@ bio.display = function(){
 	var formattedWelcome = HTMLwelcomeMsg.replace("%data%", bio.welcome);
 	var formattedContact = HTMLcontactGeneric.replace("%contact%", "eMail").replace("%data%", bio.contact.email);
 	var formattedMobile = HTMLmobile.replace("%data%", bio.contact.phone);
-	var formattedLocation = HTMLlocation.replace("%data%", bio.contact.location);
-	var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
+	var formattedLocation = HTMLlocation.replace("%data%", bio.contact.city);
 	var formattedSkills = HTMLskills.replace("%data%", bio.skills);
 	var formattedPict = HTMLbioPic.replace("%data%", bio.bioPict);
 	$("#header").prepend(formattedName);
@@ -38,22 +38,140 @@ bio.display = function(){
 }
 bio.display();
 
-//Create work object
+
+//education object -----------------------------------------------------//
+var education = {
+	"schools" : [{
+			"name" : "OCAD University",
+			"city" : "Toronto, Canada",
+			"location" : "100 McCaul Street, Toronto, Canada",
+			"degree" : "Bachelor of Design (BDes)",
+			"major" : "Graphic Design",
+			"dates" : 2007,
+			"url" : "http://www.ocadu.ca/academics/undergraduate/graphic-design.htm"
+		}],
+	"elected" : [{
+			"name" : "Akendi",
+			"city" : "Toronto, Canada",
+			"location" : "30 Duncan Street, Toronto, Canada",
+			"cert" : "Certification",
+			"course" : "UX Design",
+			"license" : "CXD-901-15-129",
+			"dates" : 2015,
+			"url" : "http://www.akendi.ca/ux-training/"
+		}],
+	"onlineCourses" : [{
+			"school" : "Udacity",
+			"title" : "Front-End Web Developer Nanodegree",
+			"dates" : 2015,
+			"url" : "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
+		}]
+};
+education.display = function(){
+	$('#education').append(HTMLschoolStart);
+
+	//check to see if it is schools, certifications, or onlineCourses
+		for(var key in education){
+	    	if(key == "schools"){
+	    		var mySchools = education.schools;
+				//Write a for-in loop that iterates over all the schools in my education.
+				for (var i=0; i<education.schools.length; i++) {
+					for(var key in education.schools){
+						var schoolItem = education.schools[i];						
+					 	var formattedschoolName = HTMLschoolName.replace('%data%', schoolItem.name);
+					 	var formattedschoolLocation = HTMLschoolLocation.replace("%data%", schoolItem.city);	
+					 	var formattedschoolDates = HTMLschoolDates.replace('%data%', schoolItem.dates);
+					 	var formattedschoolDegree = HTMLschoolDegree.replace('%data%', schoolItem.degree);
+					 	var formattedschoolMajor = HTMLschoolMajor.replace('%data%', schoolItem.major);
+					 	var formattedonlineURL = HTMLonlineURL.replace('%data%', schoolItem.url);
+					}
+			 	$('.education-entry:last').append(formattedschoolName);
+			 	$('.education-entry:last').append(formattedschoolLocation);
+			 	$('.education-entry:last').append(formattedschoolDates);
+			 	$('.education-entry:last').append(formattedschoolDegree); 	
+			 	$('.education-entry:last').append(formattedschoolMajor);
+			 	$('.education-entry:last').append(formattedonlineURL);
+				}
+	    	}
+	    	else if(key == "elected"){
+	    		var myElected = education.elected;
+				//Write a for-in loop that iterates over all the elected in my education.
+				for (var i=0; i<education.elected.length; i++) {
+					for(var key in education.elected){
+						var electedItem = education.elected[i];
+					 	var formattedelectedName = HTMLelectedName.replace('%data%', electedItem.name);
+					 	var formattedelectedLocation = HTMLelectedLocation.replace("%data%", electedItem.city);	
+					 	var formattedelectedDates = HTMLelectedDates.replace('%data%', electedItem.dates);
+					 	var formattedelectedCourse = HTMLelectedCourse.replace('%data%', electedItem.course);
+					 	var formattedelectedCert = HTMLelectedCert.replace('%data%', electedItem.cert);
+					 	var formattedelectedLicense = HTMLelectedLicense.replace('%data%', electedItem.license);
+					 	var formattedonlineURL = HTMLonlineURL.replace('%data%', electedItem.url);
+					}
+			 	$('.education-entry:last').append(formattedelectedName);
+			 	$('.education-entry:last').append(formattedelectedLocation);
+			 	$('.education-entry:last').append(formattedelectedDates);
+			 	$('.education-entry:last').append(formattedelectedCourse); 	
+			 	$('.education-entry:last').append(formattedelectedCert);
+			 	$('.education-entry:last').append(formattedelectedLicense);
+			 	$('.education-entry:last').append(formattedonlineURL);
+				}
+	    	}
+	    	else if(key == "onlineCourses"){
+	    		//Write a for-in loop that iterates over all the onlineCourses in my education.
+				for (var i=0; i<education.onlineCourses.length; i++) {
+					for(var key in education.onlineCourses){
+						var onlineCoursesItem = education.onlineCourses[i];
+					 	var formattedonlineCoursesSchool = HTMLonlineSchool.replace('%data%', onlineCoursesItem.school);
+					 	var formattedonlineCoursesDates = HTMLonlineDates.replace('%data%', onlineCoursesItem.dates);
+					 	var formattedonlineCoursesTitle = HTMLonlineSchool.replace('%data%', onlineCoursesItem.title);
+					 	var formattedonlineURL = HTMLonlineURL.replace('%data%', onlineCoursesItem.url);
+					}
+			 	$('.education-entry:last').append(HTMLonlineClasses);
+			 	$('.education-entry:last').append(formattedonlineCoursesSchool);
+			 	$('.education-entry:last').append(formattedonlineCoursesDates);
+			 	$('.education-entry:last').append(formattedonlineCoursesTitle); 	
+			 	$('.education-entry:last').append(formattedonlineURL);
+				}	
+	    	}
+		}
+}
+education.display();
+
+
+//work object -----------------------------------------------------//
 var work = {
 	"jobs" : [
 		{
 			"employer" : "Hofland",
 			"title" : "Graphic Design & Web Development Specialist",
-			"location" : "Toronto, Canada",
+			"city" : "Mississauga, Canada",
+			"location" : "6695 Pacific Circle, Mississauga ON, Canada",
 			"dates" : "Dec, 2014-Aug, 2015",
 			"description" : "Executed print, event and online graphic materials using the latest Adobe CC."
 		},
 		{
 			"employer" : "Quilt9",
 			"title" : "Independent Graphic Designer",
+			"city" : "Toronto, Canada",
 			"location" : "Toronto, Canada",
-			"dates" : "Dec, 2014-Aug, 2015",
+			"dates" : "Jan, 2011-Nov, 2014",
 			"description" : "Created online marketing banners, e-blast campaign, infographic, graphic elements for social marketing campaigns on mobile platform and wireframe with minimal supervision."	
+		},
+		{
+			"employer" : "Bee Networx",
+			"title" : "Senior Designer",
+			"city" : "Vaughan, Canada",
+			"location" : "668 Millway Avenue, Vaughan, Canada",
+			"dates" : "Feb, 2010-Dec, 2010",
+			"description" : "Created online marketing banners, e-blast campaign, infographic, graphic elements for social marketing campaigns on mobile platform and wireframe with minimal supervision."
+		},
+		{
+			"employer" : "Geminare",
+			"title" : "Senior Designer",
+			"city" : "Toronto, Canada",
+			"location" : "225 Richmond St. West, Toronto, Canada",
+			"dates" : "Aug, 2008-Jan, 2010",
+			"description" : "Created online marketing banners, e-blast campaign, infographic, graphic elements for social marketing campaigns on mobile platform and wireframe with minimal supervision."
 		}
 	]
 };
@@ -81,7 +199,7 @@ work.display = function() {
             var formattedEmployer = HTMLworkEmployer.replace("%data%", jobItem.employer);
 		    var formattedTitle = HTMLworkTitle.replace("%data%", jobItem.title);	
 	      	var formattedDates = HTMLworkDates.replace("%data%", jobItem.dates);	
-	      	var formattedLocation = HTMLworkLocation.replace("%data%", jobItem.location);	
+	      	var formattedLocation = HTMLworkLocation.replace("%data%", jobItem.city);	
 	      	var formattedDescription = HTMLworkDescription.replace("%data%", jobItem.description);
 	      	//4. append a concatenation of employer and title each to the element with class work-entry:last.
        		var employerTitle = formattedEmployer + formattedTitle;
@@ -95,7 +213,8 @@ work.display = function() {
 };
 work.display();
 
-//Create project object
+
+//project object -----------------------------------------------------//
 var allProjects = {
     "projects": [
         {
@@ -151,46 +270,8 @@ projects.display();
 
 
 
-//Create education object
-var education = {
-	"schools" : [
-		{
-			"name" : "OCAD U",
-			"city" : "Toronto, Canada",
-			"degree" : "BDes",
-			"major" : "Communication Design"
-		},
-		{
-			"name" : "Akendi",
-			"city" : "Toronto, Canada",
-			"cert" : "Certification",
-			"course" : "UX Design",
-			"license" : "CXD-901-15-129",
-			"link" : "http://www.akendi.ca/ux-training/"
-		}
-	],
-	"onlineCourses" : [
-		{
-			"title" : "Front-End Web Developer Nanodegree",
-			"school" : "Udacity",
-			"dates" : 2015,
-			"link" : "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
-		}
-	]
-};
-
 //To see a map
 $('#mapDiv').append(googleMap);
 
-//Create two new variables for my Name and Role
-
-
-//var formattedSchool = HTMLschoolName.replace("%data%", education.schools.name(0));
-//var formattedPosition = HTMLworkTitle.replace("%data%", work.position);
-
-//Format my Name and Role and either append or prepend to the page
-
-//$("#header").append(formattedSchool);
-//$("#header").append(formattedPosition);
 
 
